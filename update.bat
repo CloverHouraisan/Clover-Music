817e5ac987dd29305a87efbc98eddbc8e94b4ba9
CloverHouraisan <clover.houraisan@gmail.com>
2020-02-16 01:04:47 -0500

    Updated WBaWC, HSiFS, and UFO

--- update.bat
+++ update.bat
@@ -1,2 +1,2 @@
-C:\Users\clove\AppData\Local\Programs\Python\Python37\python.exe ..\scripts\repo_update.py
+C:\Users\clove\AppData\Local\Programs\Python\Python37\python.exe D:\Downloads\thcrap_brliron\Clover\scripts\repo_update.py
 pause
