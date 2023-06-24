f59ab69ba94096d3dc38c64a071a04e7e9f4b2ac
CloverHouraisan <50288358+CloverHouraisan@users.noreply.github.com>
2023-06-24 04:23:27 -0400

    Further updates
    Fixed Nostalgic Blood of the East (broke the loop previously, sorry!) and updated Extend Ash.

--- music_mod/patch.js
+++ music_mod/patch.js
@@ -1,11 +1,11 @@
 {
-  "dependencies": [
-    "nmlgc/base_tsa",
-    "nmlgc/base_tasofro"
-  ],
-  "id": "music_mod",
-  "servers": [
-    "https://mirrors.thpatch.net/Clover/music_mod/"
-  ],
-  "title": "Clover's Music Mods"
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/base_tasofro"
+	],
+	"id": "music_mod",
+	"servers": [
+		"https://mirrors.thpatch.net/Clover/music_mod/"
+	],
+	"title": "Clover's Music Mods"
 }
